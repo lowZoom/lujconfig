20d8bf50b69c1e4f1355452a7e17aab123f10f16
luj <david_lu_st@163.com>
2018-09-27 14:20:47 +0800

    添加maven插件工程

---
+++ lujconfig-maven-plugin/src/main/groovy/luj/config/maven/plugin/excel/ExcelToJson.groovy
@@ -0,0 +1,11 @@
+package luj.config.maven.plugin.excel
+
+import luj.groovy.AutoCtor
+
+@AutoCtor
+class ExcelToJson {
+
+  void execute() {
+    println('hehe' * 10)
+  }
+}
---
+++ lujconfig-maven-plugin/src/main/java/luj/config/maven/plugin/ExcelToJsonMojo.java
@@ -0,0 +1,21 @@
+package luj.config.maven.plugin;
+
+import luj.config.maven.plugin.excel.ExcelToJson;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * @see <a href="https://github.com/groovy/groovy-eclipse/wiki/Groovy-Eclipse-Maven-plugin#do-almost-nothing">本类存在原因</a>
+ */
+@Mojo(name = "excel-to-json")
+final class ExcelToJsonMojo extends AbstractMojo {
+
+  @Parameter(defaultValue = "${project}", readonly = true)
+  private MavenProject project;
+
+  public void execute() {
+    new ExcelToJson().execute();
+  }
+}

45f482502c17483491008bae66187d2443c8e9ad
luj <david_lu_st@163.com>
2018-11-15 09:32:43 +0800

    增加断言

--- lujconfig/src/main/java/luj/config/internal/cache/ConfigCacheImpl.java
+++ lujconfig/src/main/java/luj/config/internal/cache/ConfigCacheImpl.java
@@ -1,12 +1,14 @@
 package luj.config.internal.cache;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 
 final class ConfigCacheImpl implements ConfigCache {
 
   ConfigCacheImpl(Map<Class<?>, Map<String, Object>> typeMap) {
-    _typeMap = typeMap;
+    _typeMap = checkNotNull(typeMap);
   }
 
   @SuppressWarnings("unchecked")

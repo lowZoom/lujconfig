55af80f0a8c66a7a9f74f25075cabff124478be2
luj <luj@luj.luj>
2020-03-28 00:07:39 +0800

    增加基本数据类型支持

--- lujconfig-ex/src/main/java/luj/config/ex/internal/generate/extract/header/column/ColumnImpl.java
+++ lujconfig-ex/src/main/java/luj/config/ex/internal/generate/extract/header/column/ColumnImpl.java
@@ -1,6 +1,7 @@
 package luj.config.ex.internal.generate.extract.header.column;
 
 import luj.config.ex.api.extract.HeaderColumnExtractor;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 final class ColumnImpl implements HeaderColumnExtractor.Column {
@@ -18,7 +19,8 @@ final class ColumnImpl implements HeaderColumnExtractor.Column {
 
   @Override
   public HeaderColumnExtractor.Cell getCell(int row) {
-    return new CellImpl(_poiSheet.getRow(row).getCell(_excelColumn));
+    Row rowObj = _poiSheet.getRow(row);
+    return new CellImpl(rowObj.getCell(_excelColumn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK));
   }
 
   private final int _dataBeginColumn;
--- lujconfig-ex/src/main/java/luj/config/ex/internal/generate/extract/row/FieldValueReader.java
+++ lujconfig-ex/src/main/java/luj/config/ex/internal/generate/extract/row/FieldValueReader.java
@@ -1,5 +1,7 @@
 package luj.config.ex.internal.generate.extract.row;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 
@@ -15,25 +17,52 @@ final class FieldValueReader {
       return null;
     }
 
+    CellType actualType = getCellType();
+    if (actualType == CellType.STRING && _poiCell.getStringCellValue().isEmpty()) {
+      return null;
+    }
+
     if (_dataType == int.class) {
-      return getIntValue();
+      return getIntValue(actualType);
     }
 
-    return getStringValue();
-  }
-
-  private int getIntValue() {
-    if (_poiCell.getCellTypeEnum() == CellType.STRING) {
-      return Integer.parseInt(_poiCell.getStringCellValue());
+    if (_dataType == double.class || _dataType == float.class) {
+      return getDoubleValue(actualType);
     }
-    return (int) Math.round(_poiCell.getNumericCellValue());
+
+    if (_dataType == boolean.class) {
+      return getIntValue(actualType) > 0;
+    }
+
+    return getStringValue(actualType);
   }
 
-  private String getStringValue() {
-    if (_poiCell.getCellTypeEnum() == CellType.NUMERIC) {
+  private int getIntValue(CellType cellType) {
+    return (int) Math.round(getDoubleValue(cellType));
+  }
+
+  private double getDoubleValue(CellType cellType) {
+    if (cellType == CellType.STRING) {
+      String stringVal = _poiCell.getStringCellValue();
+      checkState(!stringVal.isEmpty());
+      return Double.parseDouble(stringVal);
+    }
+    return _poiCell.getNumericCellValue();
+  }
+
+  private String getStringValue(CellType cellType) {
+    if (cellType == CellType.NUMERIC) {
       return Double.toString(_poiCell.getNumericCellValue());
     }
     return _poiCell.getStringCellValue();
+  }
+
+  private CellType getCellType() {
+    CellType cellType = _poiCell.getCellTypeEnum();
+    if (cellType == CellType.FORMULA) {
+      return _poiCell.getCachedFormulaResultTypeEnum();
+    }
+    return cellType;
   }
 
   private final Cell _poiCell;

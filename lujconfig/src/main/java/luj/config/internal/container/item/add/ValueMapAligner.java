7cdc470cbb949a2c1d8f097fa82a14c95d4b8920
luj <luj@luj.luj>
2022-07-27 14:10:49 +0800

    完善值读取逻辑

--- lujconfig/src/main/java/luj/config/internal/container/item/add/ValueMapAligner.java
+++ lujconfig/src/main/java/luj/config/internal/container/item/add/ValueMapAligner.java
@@ -2,24 +2,27 @@ package luj.config.internal.container.item.add;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import com.google.common.base.Defaults;
-import com.google.common.collect.ImmutableMap;
 import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Map;
+import luj.ava.collection.map.MapX;
 
+/**
+ * 把实际值的类型跟代码声明相对应
+ */
 enum ValueMapAligner {
   GET;
 
   Map<String, Object> align(Map<String, Object> input, Class<?> type) {
     checkArgument(type.isInterface(), type.getName());
 
-    ImmutableMap.Builder<String, Object> result = ImmutableMap.builder();
+    MapX.Builder<String, Object> result = MapX.builder();
     for (Method m : type.getMethods()) {
       String fieldName = m.getName();
       Object oldValue = input.get(fieldName);
 
-      result.put(fieldName.intern(), convertValue(oldValue, m.getReturnType()));
+      Object newValue = convertValue(oldValue, m.getReturnType());
+      result.put(fieldName.intern(), newValue);
     }
 
     return result.build();

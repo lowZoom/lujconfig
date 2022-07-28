9c753f3e61671954b67c3f1530bc5473a338b3ee
luj <luj@luj.luj>
2022-07-28 18:04:54 +0800

    优化excel扫描逻辑

--- lujconfig-ex/src/main/java/luj/config/ex/internal/generate/ExcelPathCollector.java
+++ lujconfig-ex/src/main/java/luj/config/ex/internal/generate/ExcelPathCollector.java
@@ -1,41 +1,23 @@
 package luj.config.ex.internal.generate;
 
-import static java.util.stream.Collectors.toList;
-
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import luj.ava.file.path.PathX;
 
-final class ExcelPathCollector {
+public class ExcelPathCollector {
 
-  ExcelPathCollector(Path excelDir) {
+  public ExcelPathCollector(Path excelDir) {
     _excelDir = excelDir;
   }
 
   public List<Path> collect() {
-    Stream<Path> walker = startWalk();
-    List<Path> excelList = walker
-        .filter(p -> Files.isRegularFile(p))
-        .map(PathX::of)
+    return PathX.of(_excelDir).walk(s -> s
+        .filter(PathX::isRegularFile)
         .filter(p -> p.getFileName().endsWith(".xlsx"))
         .filter(p -> !p.getFileName().startsWith("~$"))
         .map(PathX::asPath)
-        .collect(toList());
-
-    walker.close();
-    return excelList;
-  }
-
-  private Stream<Path> startWalk() {
-    try {
-      return Files.walk(_excelDir);
-
-    } catch (IOException e) {
-      throw new UnsupportedOperationException(e);
-    }
+        .collect(Collectors.toList()));
   }
 
   private final Path _excelDir;
